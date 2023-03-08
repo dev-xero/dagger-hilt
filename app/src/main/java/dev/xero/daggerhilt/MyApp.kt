2d7ac8d2065564d50c0b6136d99c9c0bfcf9fa04
Xero kun <elo48858@gmail.com>
2023-03-08 13:58:53 +0100

    `✨UPDATE` Setup Hilt Android App

---
+++ app/src/main/java/dev/xero/daggerhilt/MyApp.kt
@@ -0,0 +1,8 @@
+package dev.xero.daggerhilt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application() {
+}
--- app/src/main/java/dev/xero/daggerhilt/data/repository/MyRepositoryImpl.kt
+++ app/src/main/java/dev/xero/daggerhilt/data/repository/MyRepositoryImpl.kt
@@ -1,10 +1,12 @@
 package dev.xero.daggerhilt.data.repository
 
+import android.app.Application
 import dev.xero.daggerhilt.data.remote.TestAPI
 import dev.xero.daggerhilt.domain.repository.MyRepository
 
 class MyRepositoryImpl(
-	private val api: TestAPI
+	private val api: TestAPI,
+	private val appContext: Application
 ) : MyRepository {
 	override suspend fun networkCall() {
 
