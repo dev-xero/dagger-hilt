4353a2ec56e987336c5e9aff155f92740c492bff
Xero kun <elo48858@gmail.com>
2023-03-08 14:04:41 +0100

    `✨UPDATE` Used the AppContext

--- app/src/main/java/dev/xero/daggerhilt/data/repository/MyRepositoryImpl.kt
+++ app/src/main/java/dev/xero/daggerhilt/data/repository/MyRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package dev.xero.daggerhilt.data.repository
 
 import android.app.Application
+import dev.xero.daggerhilt.R
 import dev.xero.daggerhilt.data.remote.TestAPI
 import dev.xero.daggerhilt.domain.repository.MyRepository
 
@@ -8,6 +9,11 @@ class MyRepositoryImpl(
 	private val api: TestAPI,
 	private val appContext: Application
 ) : MyRepository {
+	init {
+		val appName = appContext.getString(R.string.app_name)
+		println("Hello, the app name is: $appName")
+	}
+
 	override suspend fun networkCall() {
 
 	}
