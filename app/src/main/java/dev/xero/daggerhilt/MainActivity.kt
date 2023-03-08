1f258b75deb50e88436ebf6dbf23cd12b45556ab
Xero kun <elo48858@gmail.com>
2023-03-08 13:56:23 +0100

    `✨UPDATE` Added HiltViewModel to MainActivity

--- app/src/main/java/dev/xero/daggerhilt/MainActivity.kt
+++ app/src/main/java/dev/xero/daggerhilt/MainActivity.kt
@@ -10,13 +10,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import dev.xero.daggerhilt.ui.theme.DaggerHiltTheme
+import dev.xero.daggerhilt.viewmodel.AppViewModel
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
 			DaggerHiltTheme {
+				val viewModel = hiltViewModel<AppViewModel>()
 			}
 		}
 	}
